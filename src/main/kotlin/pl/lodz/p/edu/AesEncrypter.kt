cea1230efbfb763aff91c2b45ac1237e20e0abbb
Łukasz Moskwin <>
2024-03-03 17:09:26 +0100

    Aes addRoundKey, not tested

--- src/main/kotlin/pl/lodz/p/edu/AesEncrypter.kt
+++ src/main/kotlin/pl/lodz/p/edu/AesEncrypter.kt
@@ -1,10 +1,18 @@
 package pl.lodz.p.edu
 
-import java.security.SecureRandom
+import kotlin.experimental.xor
 
 class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
     // Tutaj tylko szyfrowanie i deszyfrowanie danych w postaci bajtów.
-
+    private fun addRoundKey(state: Array<ByteArray>, w: Array<ByteArray>, round: Int): Array<ByteArray> {
+        val tmp = arrayOf(ByteArray(4))
+        for (i in 0..3){
+            for (j in 0..3){
+                tmp[j][i] = state[j][i] xor w[4*round+i][j]
+            }
+        }
+        return tmp
+    }
     override fun encryptData(data: ByteArray): ByteArray {
         throw NotImplementedError("AES encryption is not implemented yet!")
     }
