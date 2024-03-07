583b4eee2570e012397d7dc6315f5df15f228849
Łukasz Moskwin <lukasz_moskwin@o2.pl>
2024-03-07 23:43:14 +0100

    Decryption implementation with inverted functions, Encryption or Decryption not working properly

--- src/main/kotlin/pl/lodz/p/edu/AesEncrypter.kt
+++ src/main/kotlin/pl/lodz/p/edu/AesEncrypter.kt
@@ -10,7 +10,7 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
         val tmp = Array(4) { UByteArray(4) }
         for (i in 0..3){
             for (j in 0..3){
-                tmp[j][i] = state[j][i] xor roundKeys[4*round+i][j]
+                tmp[j][i] = state[j][i] xor roundKeys[(4*round)+i][j]
             }
         }
         return tmp
@@ -18,9 +18,19 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
 
     private fun subBytes(state: Array<UByteArray>): Array<UByteArray> {
         val tmp = Array<UByteArray>(4){UByteArray(4)}
-        for (i in 0..15) {
-            for (j in 0..15) {
+        for (i in 0..3) {
+            for (j in 0..3) {
                 tmp[i][j] = generateSBox()[state[i][j].toInt()]
+            }
+        }
+        return tmp
+    }
+
+    private fun invSubBytes(state: Array<UByteArray>): Array<UByteArray> {
+        val tmp = Array<UByteArray>(4){UByteArray(4)}
+        for (i in 0..3) {
+            for (j in 0..3) {
+                tmp[i][j] = generateInvSBox()[state[i][j].toInt()]
             }
         }
         return tmp
@@ -35,25 +45,58 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
         return tmp
     }
 
-    private fun mixColumns(state: Array<UByteArray>): Array<UByteArray> {
-        fun mulBy2(byte: UByte): UByte{
-            var tmp = byte.toInt()
-            tmp = tmp shl 1
-            if (tmp>0xFF) {
-                tmp = tmp xor 0x1B
-            }
-            return tmp.toUByte()
+    private fun invShiftRows(state: Array<UByteArray>): Array<UByteArray> {
+        val tmp = Array(4){UByteArray(4)}
+        for (i in 0..3) {
+            for (j in 0..3)
+                tmp[i][j] = state[i][(i-j).mod(4)]
         }
-        fun mulBy3(byte: UByte): UByte{
-            return mulBy2(byte) xor byte
+        return tmp
+    }
+
+    // Całkowicie szczerze ta funkcja jest przekopiowana z internetu i do tego
+    // przekonwertowana przez intellij z C# (bo wykryło jako java xD)
+    // Dla mnie to jest czarna magia i nie rozumiem co się tu dzieje
+    // Całkiem możliwe że ta funkcja nie będzie działać poprawnie
+    private fun gMul(a: UByte, b: UByte): UByte {
+        var a = a
+        var b = b
+        var p: UByte = 0u
+
+        for (counter in 0..7) {
+            if ((b.toInt() and 1) != 0) {
+                p = (p.toInt() xor a.toInt()).toUByte()
+            }
+
+            val hiBitSet: Boolean = (a.toInt() and 0x80) != 0
+            a = (a.toInt() shl 1).toUByte()
+            if (hiBitSet) {
+                a = (a.toInt() xor 0x1B).toUByte() /* x^8 + x^4 + x^3 + x + 1 */
+            }
+            b = (b.toInt() shr 1).toUByte()
         }
 
+        return p
+    }
+
+    private fun mixColumns(state: Array<UByteArray>): Array<UByteArray> {
         val tmp = Array(4) { UByteArray(4) }
         for (i in 0..3) {
-            tmp[0][i] = mulBy2(state[0][i]) xor mulBy3(state[1][i]) xor state[2][i] xor state[3][i]
-            tmp[1][i] = state[0][i] xor mulBy2(state[1][i]) xor mulBy3(state[2][i]) xor state[3][i]
-            tmp[2][i] = state[0][i] xor state[1][i] xor mulBy2(state[2][i]) xor mulBy3(state[3][i])
-            tmp[3][i] = mulBy3(state[0][i]) xor state[1][i] xor state[2][i] xor mulBy2(state[3][i])
+            tmp[0][i] = gMul(state[0][i], 2u) xor gMul(state[1][i], 3u) xor state[2][i] xor state[3][i]
+            tmp[1][i] = state[0][i] xor gMul(state[1][i], 2u) xor gMul(state[2][i], 3u) xor state[3][i]
+            tmp[2][i] = state[0][i] xor state[1][i] xor gMul(state[2][i], 2u) xor gMul(state[3][i], 3u)
+            tmp[3][i] = gMul(state[0][i], 3u) xor state[1][i] xor state[2][i] xor gMul(state[3][i], 2u)
+        }
+        return tmp
+    }
+
+    private fun invMixColumns(state: Array<UByteArray>): Array<UByteArray> {
+        val tmp = Array(4) { UByteArray(4) }
+        for (i in 0..3) {
+            tmp[0][i] = gMul(state[0][i], 14u) xor gMul(state[1][i], 11u) xor gMul(state[2][i], 13u) xor gMul(state[3][i], 9u)
+            tmp[1][i] = gMul(state[0][i], 9u) xor gMul(state[1][i], 14u) xor gMul(state[2][i], 11u) xor gMul(state[3][i], 13u)
+            tmp[2][i] = gMul(state[0][i], 13u) xor gMul(state[1][i], 9u) xor gMul(state[2][i], 14u) xor gMul(state[3][i], 11u)
+            tmp[3][i] = gMul(state[0][i], 11u) xor gMul(state[1][i], 13u) xor gMul(state[2][i], 9u) xor gMul(state[3][i], 14u)
         }
         return tmp
     }
@@ -62,8 +105,9 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
         if (data.size != 16)
             throw IllegalArgumentException("Block of data must be 16 byte long")
 
-        var state = arrayOf(data.copyOfRange(0, 3), data.copyOfRange(4, 7), data.copyOfRange(8, 11), data.copyOfRange(12, 15))
+        var state = arrayOf(data.copyOfRange(0, 4), data.copyOfRange(4, 8), data.copyOfRange(8, 12), data.copyOfRange(12, 16))
         val roundKeys = key.genSubKeys()
+
         state = addRoundKey(state, roundKeys, 0)
         for (round in 1..<key.getNumberOfRounds()) {
             state = subBytes(state)
@@ -73,12 +117,31 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
         }
         state = subBytes(state)
         state = shiftRows(state)
-        state = addRoundKey(state, roundKeys, 16)
+        state = addRoundKey(state, roundKeys, key.getNumberOfRounds())
+
         return state.flatMap { it.asIterable() }.toUByteArray()
     }
 
     override fun decryptData(data: UByteArray): UByteArray {
-        throw NotImplementedError("AES decryption is not implemented yet!")
+        if (data.size != 16)
+            throw IllegalArgumentException("Block of data must be 16 byte long")
+
+        var state = arrayOf(data.copyOfRange(0, 4), data.copyOfRange(4, 8), data.copyOfRange(8, 12), data.copyOfRange(12, 16))
+        val roundKeys = key.genSubKeys()
+
+        state = addRoundKey(state, roundKeys, key.getNumberOfRounds())
+        state = invShiftRows(state)
+        state = invSubBytes(state)
+
+        for (round in key.getNumberOfRounds()-1 downTo 1) {
+            state = addRoundKey(state, roundKeys, round)
+            state = invMixColumns(state)
+            state = invShiftRows(state)
+            state = invSubBytes(state)
+        }
+        state = addRoundKey(state, roundKeys, 0)
+
+        return state.flatMap { it.asIterable() }.toUByteArray()
     }
 
     companion object {
@@ -99,6 +162,25 @@ class AesEncrypter(val key: AesKey) : Encrypter, Decrypter {
             0x70u, 0x3Eu, 0xB5u, 0x66u, 0x48u, 0x03u, 0xF6u, 0x0Eu, 0x61u, 0x35u, 0x57u, 0xB9u, 0x86u, 0xC1u, 0x1Du, 0x9Eu,
             0xE1u, 0xF8u, 0x98u, 0x11u, 0x69u, 0xD9u, 0x8Eu, 0x94u, 0x9Bu, 0x1Eu, 0x87u, 0xE9u, 0xCEu, 0x55u, 0x28u, 0xDFu,
             0x8Cu, 0xA1u, 0x89u, 0x0Du, 0xBFu, 0xE6u, 0x42u, 0x68u, 0x41u, 0x99u, 0x2Du, 0x0Fu, 0xB0u, 0x54u, 0xBBu, 0x16u
+        )
+        
+        fun generateInvSBox(): UByteArray = ubyteArrayOf(
+            0x52u, 0x09u, 0x6au, 0xd5u, 0x30u, 0x36u, 0xa5u, 0x38u, 0xbfu, 0x40u, 0xa3u, 0x9eu, 0x81u, 0xf3u, 0xd7u, 0xfbu,
+            0x7cu, 0xe3u, 0x39u, 0x82u, 0x9bu, 0x2fu, 0xffu, 0x87u, 0x34u, 0x8eu, 0x43u, 0x44u, 0xc4u, 0xdeu, 0xe9u, 0xcbu,
+            0x54u, 0x7bu, 0x94u, 0x32u, 0xa6u, 0xc2u, 0x23u, 0x3du, 0xeeu, 0x4cu, 0x95u, 0x0bu, 0x42u, 0xfau, 0xc3u, 0x4eu,
+            0x08u, 0x2eu, 0xa1u, 0x66u, 0x28u, 0xd9u, 0x24u, 0xb2u, 0x76u, 0x5bu, 0xa2u, 0x49u, 0x6du, 0x8bu, 0xd1u, 0x25u,
+            0x72u, 0xf8u, 0xf6u, 0x64u, 0x86u, 0x68u, 0x98u, 0x16u, 0xd4u, 0xa4u, 0x5cu, 0xccu, 0x5du, 0x65u, 0xb6u, 0x92u,
+            0x6cu, 0x70u, 0x48u, 0x50u, 0xfdu, 0xedu, 0xb9u, 0xdau, 0x5eu, 0x15u, 0x46u, 0x57u, 0xa7u, 0x8du, 0x9du, 0x84u,
+            0x90u, 0xd8u, 0xabu, 0x00u, 0x8cu, 0xbcu, 0xd3u, 0x0au, 0xf7u, 0xe4u, 0x58u, 0x05u, 0xb8u, 0xb3u, 0x45u, 0x06u,
+            0xd0u, 0x2cu, 0x1eu, 0x8fu, 0xcau, 0x3fu, 0x0fu, 0x02u, 0xc1u, 0xafu, 0xbdu, 0x03u, 0x01u, 0x13u, 0x8au, 0x6bu,
+            0x3au, 0x91u, 0x11u, 0x41u, 0x4fu, 0x67u, 0xdcu, 0xeau, 0x97u, 0xf2u, 0xcfu, 0xceu, 0xf0u, 0xb4u, 0xe6u, 0x73u,
+            0x96u, 0xacu, 0x74u, 0x22u, 0xe7u, 0xadu, 0x35u, 0x85u, 0xe2u, 0xf9u, 0x37u, 0xe8u, 0x1cu, 0x75u, 0xdfu, 0x6eu,
+            0x47u, 0xf1u, 0x1au, 0x71u, 0x1du, 0x29u, 0xc5u, 0x89u, 0x6fu, 0xb7u, 0x62u, 0x0eu, 0xaau, 0x18u, 0xbeu, 0x1bu,
+            0xfcu, 0x56u, 0x3eu, 0x4bu, 0xc6u, 0xd2u, 0x79u, 0x20u, 0x9au, 0xdbu, 0xc0u, 0xfeu, 0x78u, 0xcdu, 0x5au, 0xf4u,
+            0x1fu, 0xddu, 0xa8u, 0x33u, 0x88u, 0x07u, 0xc7u, 0x31u, 0xb1u, 0x12u, 0x10u, 0x59u, 0x27u, 0x80u, 0xecu, 0x5fu,
+            0x60u, 0x51u, 0x7fu, 0xa9u, 0x19u, 0xb5u, 0x4au, 0x0du, 0x2du, 0xe5u, 0x7au, 0x9fu, 0x93u, 0xc9u, 0x9cu, 0xefu,
+            0xa0u, 0xe0u, 0x3bu, 0x4du, 0xaeu, 0x2au, 0xf5u, 0xb0u, 0xc8u, 0xebu, 0xbbu, 0x3cu, 0x83u, 0x53u, 0x99u, 0x61u,
+            0x17u, 0x2bu, 0x04u, 0x7eu, 0xbau, 0x77u, 0xd6u, 0x26u, 0xe1u, 0x69u, 0x14u, 0x63u, 0x55u, 0x21u, 0x0cu, 0x7du
         )
     }
 }
